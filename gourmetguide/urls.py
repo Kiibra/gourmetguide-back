af3dfa392958ca993f86a3be77bf6b96cb0943e7
kiibra <kibra99@gmail.com>
2024-07-17 15:12:24 -0400

    add urls& view function for add_recipe

--- gourmetguide/urls.py
+++ gourmetguide/urls.py
@@ -21,4 +21,5 @@ from gourmetguide import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipes/', views.recipes, name='recipes'),
+    path('recipes/addrecipe/', views.add_recipe, name='add-recipe'),
 ]
--- gourmetguide/views.py
+++ gourmetguide/views.py
@@ -1,11 +1,20 @@
 from gourmetguide.models import Recipe
 from gourmetguide.serializers import RecipeSerializer
 from rest_framework.decorators import api_view
+from rest_framework import status
 from django.http import JsonResponse
 
 
 @api_view(['GET'])
 def recipes (request):
-    data = Recipe.objects.values()
+    data = Recipe.objects.all()
     serializer = RecipeSerializer(data, many=True)
     return JsonResponse({'recipes': serializer.data})
+
+@api_view(['POST'])
+def add_recipe(request):
+    serializer = RecipeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse({'message': 'Recipe added successfully!', 'data': serializer.data}, status=status.HTTP_201_CREATED)
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
